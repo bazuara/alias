f7199bad051dc9bf6898534f3b917dc951cb4df0
Bruno Azuara <bruno.azuara@gmail.com>
2020-01-07 15:33:50 +0100

    Subida automática

--- alias.zsh
+++ alias.zsh
@@ -4,9 +4,13 @@ alias up='cd .. && ll'
 alias reload='vim ~/.oh-my-zsh/custom/alias.zsh && source ~/.oh-my-zsh/custom/alias.zsh'
 alias ll='ls -lah'
 alias tree="pwd && find . -not -path '*/\.git/*' -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
-alias gp="git add . && git commit -m 'Subida automática' && git push"
+alias gaut="git add . && git commit -m 'Subida automática' && git push"
 alias gs="git status"
 alias cd42='cd ~/Documents/42/Ecole/'
+alias cdtool='cd ~/Documents/42/Tools/'
+alias mygit='git --git-dir=.github'
+alias mygaut=" mygit add . && mygit commit -m 'Subida automática' && mygit push"
 function mkcd() { mkdir -p "$@" && cd "$@"; }
 #End of Custom Alias
+
 
