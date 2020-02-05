75d7ec7640f00d88711e724af43cf49e2243dbd3
Bruno Azuara <bruno.azuara@gmail.com>
2020-02-05 17:15:30 +0100

    changed auto git to allow messages

--- sources/.alias.zsh
+++ sources/.alias.zsh
@@ -4,12 +4,12 @@ alias up='cd .. && ll'
 alias reload='vim ~/.oh-my-zsh/custom/alias.zsh && source ~/.oh-my-zsh/custom/alias.zsh'
 alias ll='ls -lah'
 alias tree="pwd && find . -not -path '*/\.git/*' -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
-alias gaut="git add . && git commit -m 'Subida automática' && git push"
+alias gaut="git add . && git commit && git push"
 alias gs="git status"
-alias cd42='cd ~/Documents/42/Ecole/'
+alias cd42='cd ~/Documents/42/Ecole/ft_printf/bazuara'
 alias cdtool='cd ~/Documents/42/Tools/'
 alias mygit='git --git-dir=.github'
-alias mygaut=" mygit add . && mygit commit -m 'Subida automática' && mygit push"
+alias mygaut=" mygit add . && mygit commit  && mygit push"
 function mkcd() { mkdir -p "$@" && cd "$@"; }
 #End of Custom Alias
 
